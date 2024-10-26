bebae87833839132b31ad2138886718c9bc7695b
camila.suarezviltres <camila.suarezviltres@gmail.com>
2024-10-26 15:23:50 -0600

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
-import Items from "./components/Items.jsx";
+import HomePage from "./pages/HomePage/HomePage.jsx";
+import ItemPage from "./pages/ItemPage/ItemPage.jsx";
+import NewItemPage from "./pages/NewItemPage/NewItemPage.jsx";
 
 function App() {
   return (
-    <>
-      <div>
-        <h1>InCloset</h1>
-        <Items />
-      </div>
-    </>
+    <div className="main">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/item" element={<HomePage />} />
+          <Route path="/item/:id" element={<ItemPage />} />
+          <Route path="/new-item" element={<NewItemPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
