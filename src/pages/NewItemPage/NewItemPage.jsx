8e9660a150b5dce4baa0302f4811552e9999ea02
camila.suarezviltres <camila.suarezviltres@gmail.com>
2024-10-26 15:26:29 -0600

    add new item page

---
+++ src/pages/NewItemPage/NewItemPage.jsx
@@ -0,0 +1,10 @@
+function NewItemPage() {
+  
+  return (
+    <div>
+      <h1>Add new item</h1>
+      <input type="file" accept="image/*,.pdf" />
+    </div>
+  );
+}
+export default NewItemPage;
