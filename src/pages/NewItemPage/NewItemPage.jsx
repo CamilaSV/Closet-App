9ad31b88e7e2c70230c8c395b1870bfef720c87d
camila.suarezviltres <camila.suarezviltres@gmail.com>
2024-10-27 20:22:50 -0600

    allow user to add new item

--- src/pages/NewItemPage/NewItemPage.jsx
+++ src/pages/NewItemPage/NewItemPage.jsx
@@ -1,9 +1,35 @@
+import { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import ItemForm from "/src/components/ItemForm/ItemForm";
+import axios from "axios";
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
 function NewItemPage() {
-  
+  const [item, setItem] = useState({});
+  const formRef = useRef(null);
+  const navigate = useNavigate();
+
+  async function handleSubmit(formData) {
+    try {
+      await axios.post(`${BASE_URL}/item`, formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+      navigate(-1);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div>
       <h1>Add new item</h1>
-      <input type="file" accept="image/*,.pdf" />
+      <ItemForm
+        item={item}
+        setItem={setItem}
+        formRef={formRef}
+        handleSubmit={handleSubmit}
+      />
     </div>
   );
 }
